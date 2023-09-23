e8400004f03a37e14775c0e93faa265946df9252
galah92 <galaharoni92@gmail.com>
2023-09-23 10:31:23 +0300

    Added postgres connection

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("YO");
+import postgres from 'postgres';
+
+const sql = postgres({
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  username: 'postgres',
+  password: 'postgres',
+});
+
+(async () => {
+  const [{ now }] = await sql`SELECT NOW()`;
+  console.log(now);
+})();
